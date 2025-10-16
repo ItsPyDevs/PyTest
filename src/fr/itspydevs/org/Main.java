ff94a6aa51b46df867dbd087dbe16da5f0f7f8e7
ItsPyDevs <128306684+ItsPyDevs@users.noreply.github.com>
2025-10-16 18:53:40 +0200

    fix

--- src/fr/itspydevs/org/Main.java
+++ src/fr/itspydevs/org/Main.java
@@ -1,28 +1,23 @@
 package fr.itspydevs.org;
 
-import java.awt.Font;
-import java.awt.Image;
-import java.net.URL;
+import java.awt.*;
+import java.net.*;
 import javax.swing.*;
 
 public class Main {
+	static JFrame frame = new JFrame();
 	static int width = 815;
 	static int height = 640;
+	
+	static URL icône = Main.class.getResource("/icone.png");
+	static Image ico = new ImageIcon(icône).getImage();
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-
-		URL image_url = Main.class.getResource("/icone.png");
-		Image image = new ImageIcon(image_url).getImage();
-		frame.setIconImage(image);
-
+		frame.setIconImage(ico);
 		frame.setSize(width, height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
 		frame.setResizable(false);
-
-
 		frame.setTitle("Gui trop stylé sm");
 
 		JLabel label = new JLabel("Salut", SwingConstants.NORTH_EAST);
@@ -31,14 +26,27 @@ public class Main {
 		Font text_font = new Font("Ubuntu", Font.PLAIN, 30);
 		label.setFont(text_font);
 		
-		JButton bouton = new JButton("Cliquez moi!");
-		bouton.setSize(200, 50);	
-		bouton.addActionListener(e -> {
-            System.out.println("Bouton cliqué!");
-        });
-		frame.add(bouton);
+
+		Font bouton_font = new Font("Ubuntu", Font.PLAIN, 20);
+		JButton bouton = new JButton("Fetch");
+		bouton.setSize(100, 50);
 		
+		
+		bouton.addActionListener(e -> {
+			//   TODO Fetch & show the image
+			
+			System.out.println();
+        });
+		
+		bouton.setBackground(Color.RED);
+		bouton.setBorderPainted(true);
+		
+		bouton.setFont(bouton_font);
+		
+		frame.add(bouton);
 		frame.add(label);
+		
+		// frame.setAlwaysOnTop(true); 
 		
 		System.out.println("code exec");
 
